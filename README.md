aeb52becaa76bb54690328686a95a4c20603ceed
falkTX <falktx@falktx.com>
2024-11-26 11:37:37 +0100

    Add documentation
    Signed-off-by: falkTX <falktx@falktx.com>

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # libfios
+
+This is a small C library for reading/writing arbitrary data to and from a serial port.
+
+See [src/libfios.h](src/libfios.h) for the API.
+
+## Building
+
+libfios uses cmake for building and has no other dependencies (besides a working C compiler).
+
+building libfios is as simple as:
+
+```
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+```
+
+## Example
+
+### Standalone
+
+An example standalone utility is included as part of the default build, called "fios-file".
+
+```
+$ ./build/fios-file
+Usage: ./build/fios-file [r|s] [device-path|auto] [file-path]
+```
+
+The 1st argument is either "r" for "receive" or "s" for "send".
+The 2nd argument specifies the serial port to use (e.g. `/dev/ttyUSB0` on Linux and `COM5` on Windows)
+The 3rd argument specifies the file to read or write (dependending on the receive vs send mode)
+
+### Code
+
+Here is a small example on how to use this library to send a binary file.
+
+Receiver:
+
+```
+fios_serial_t* const s = fios_serial_open("/dev/ttyUSB0");
+assert(s);
+fios_file_t* const f = fios_file_receive(s, "/tmp/test.bin");
+assert(f);
+while (fios_file_idle(f, NULL))
+  sleep(1);
+fios_file_close(f);
+fios_serial_close(s);
+```
+
+Sender:
+
+```
+fios_serial_t* const s = fios_serial_open("/dev/ttyUSB1");
+assert(s);
+fios_file_t* const f = fios_file_send(s, "/path/to/bin.file");
+assert(f);
+while (fios_file_idle(f, NULL))
+  sleep(1);
+fios_file_close(f);
+fios_serial_close(s);
+```
--- src/libfios.h
+++ src/libfios.h
@@ -10,6 +10,8 @@ extern "C" {
 #include <stddef.h>
 #endif
 
+/*! define FIOS_API depending on the build type
+ */
 #ifdef FIOS_SHARED_LIBRARY
  #ifdef _WIN32
   #define FIOS_API __declspec(dllexport)
@@ -20,47 +22,92 @@ extern "C" {
  #define FIOS_API
 #endif
 
+/*! use a well known size for commands, giving enough space for a small single argument
+ */
 #define CMD_SIZE 2 /* 'w ' */ + 10 /* 0xffffffff */ + 1 /* null */
+
+/*! maximum size allowed in file APIs
+ */
 #define MAX_FILE_SIZE 0x7fffffff
+
+/*! maximum payload size, used to receive data after a command
+ */
 #define MAX_PAYLOAD_SIZE 0x2000
 
+/*! opaque API structures
+ */
 typedef struct _fios_serial_t fios_serial_t;
 typedef struct _fios_file_t fios_file_t;
 
 // --------------------------------------------------------------------------------------------------------------------
+// serial IO
 
+/*! Open the serial port at @a devpath
+ */
 FIOS_API
 fios_serial_t* fios_serial_open(const char* devpath);
 
+/*! Close a serial port
+ */
 FIOS_API
 void fios_serial_close(fios_serial_t* s);
 
 // --------------------------------------------------------------------------------------------------------------------
+// serial communication
 
+/*! read CMD_SIZE bytes from a serial port into @a cmd
+ * @note this is a blocking operation
+ */
 FIOS_API
 bool fios_serial_read_cmd(fios_serial_t* s, char cmd[CMD_SIZE]);
 
+/*! read up to MAX_PAYLOAD_SIZE bytes from a serial port into @a payload
+ * the @a payload argument must have enough space to receive the data
+ * @note this is a blocking operation
+ */
 FIOS_API
 bool fios_serial_read_payload(fios_serial_t* s, void* payload, size_t size);
 
-// NOTE null-terminated string, maximum CMD_SIZE
+/*! write up to CMD_SIZE bytes from @a cmd to a serial port
+ * the @a cmd argument can be less than CMD_SIZE, in which case the remaining bytes will be zero
+ * @note this is a blocking operation
+ */
 FIOS_API
 bool fios_serial_write_cmd(fios_serial_t* s, const char* cmd);
 
+/*! write @a size bytes from @a payload to a serial port
+ * @note this is a blocking operation
+ */
 FIOS_API
 bool fios_serial_write_payload(fios_serial_t* s, const void* payload, size_t size);
 
 // --------------------------------------------------------------------------------------------------------------------
+// file operations (using background threads)
 
+/*! prepare to receive data from a serial port into the file @a outpath
+ * a background thread is used for receiving data from the serial port and writing to the file
+ * use @fios_file_idle to query current progress and @fios_file_close when done
+ */
 FIOS_API
 fios_file_t* fios_file_receive(fios_serial_t* s, const char* outpath);
 
+/*! prepare to send data from the file @a inpath into a serial port
+ * a background thread is used for reading the file and sending data to the serial port
+ * use @fios_file_idle to query current progress and @fios_file_close when done
+ */
 FIOS_API
 fios_file_t* fios_file_send(fios_serial_t* s, const char* inpath);
 
+/*! check status of an active serial file transfer
+ * when passing a valid @a progress pointer it will indicate current progress between 0.0 and 1.0
+ * returns true if the file is still being received/sent, false if operation completed or failed
+ */
 FIOS_API
 bool fios_file_idle(fios_file_t* s, float* progress);
 
+/*! close the file operation
+ * must still be called even if @fios_file_idle returns false
+ */
 FIOS_API
 bool fios_file_close(fios_file_t* s);
 
